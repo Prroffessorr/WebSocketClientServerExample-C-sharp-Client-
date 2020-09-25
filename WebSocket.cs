f2615adbd80124cbd44ef541b120fdad0bac7cff
Andrew <mad.scientise@gmail.com>
2020-09-25 20:18:42 +0300

    add little foolproof

--- WebSocket.cs
+++ WebSocket.cs
@@ -10,30 +10,44 @@ namespace WebSocketSimpleExample
     class WebSocket
     {
         Uri serverUri = new Uri("ws://localhost:8080");
+        bool Connect_Disconnect = false;
 
         public WebSocketSharp.WebSocket WebSocketClient = null;
 
         public void Websock_Client_Node()
         {
-            try
+            if (Connect_Disconnect == false)
             {
-                WebSocketClient = new WebSocketSharp.WebSocket(serverUri.ToString());
-                WebSocketClient.OnMessage += wsServer_NewDataReceived;
-                WebSocketClient.OnOpen += wsServer_NewSessionConnected;
-                WebSocketClient.OnError += wsServer_OnError;
-                WebSocketClient.OnClose += wsServer_Disconnect;
-                WebSocketClient.Connect();
-                Console.WriteLine("Connection Open");
+                try
+                {
+                    WebSocketClient = new WebSocketSharp.WebSocket(serverUri.ToString());
+                    WebSocketClient.OnMessage += wsServer_NewDataReceived;
+                    WebSocketClient.OnOpen += wsServer_NewSessionConnected;
+                    WebSocketClient.OnError += wsServer_OnError;
+                    WebSocketClient.OnClose += wsServer_Disconnect;
+                    WebSocketClient.Connect();
+                    Console.WriteLine("Connection Open");
+                    Connect_Disconnect = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Connect_Disconnect = false;
+                }
             }
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine(ex.Message);
+                WebSocketClient.Send("User is already connected");
             }
         }
 
         private void wsServer_Disconnect(object sender, CloseEventArgs e)
         {
             Console.Write("Connection Close");
+            if(Connect_Disconnect == true)
+            {
+                Connect_Disconnect = false;
+            }
         }
         private void wsServer_OnError(object sender, WebSocketSharp.ErrorEventArgs e)
         {
